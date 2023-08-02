21ad53cecceb948c66021ee070d4e237d45853ef
dante0527 <19dante.j.lee@gmail.com>
2023-08-02 15:55:27 -0400

    added restart option

--- main.py
+++ main.py
@@ -3,46 +3,68 @@ Sleepy - sleep timer for my titi
 Author: Dante Lee
 """
 
-import tkinter as tk
-import tkinter.ttk as ttk
+from tkinter import *
 import os
 
 # Window
-root = tk.Tk()
-root.resizable(0,0)
-root.geometry("400x300+300+300")
-root.configure(bg="pink")
-root.title("Sleepy Timer")
+window = Tk()
+window.resizable(0,0)
+window.geometry("300x200+300+300")
+window.title("Sleepy Timer")
+window.configure(background="pink")
+window.grid_columnconfigure((0, 1), weight=1, uniform="equal")
 
-def sleep(option, delay):
-    option="-s"
-    os.system(f"sudo shutdown {option} +{int(delay)}")
+def sleep():
+    print(f"sudo shutdown {timer_type.get()} +{int(timer_delay.get())}")
 
 
-# Timer option
-label_delay = tk.Label(
+# Timer length option
+label_delay = Label(
+    window,
     text="Set delay",
     fg="black",
     bg="pink")
-label_delay.pack()
+label_delay.grid(column=0, row=0, padx=5, pady=5)
 
-minutes = ["15", "30", "45", "60"]
+minutes = [60, 45, 30, 15]
+timer_delay = IntVar()
+timer_delay.set(minutes[0])
+menu_delay = OptionMenu(window, timer_delay, minutes[0], *minutes[1:])
+menu_delay.config(fg="black", bg="pink", activeforeground="black")
+menu_delay.grid(column=0, row=1, padx=5, pady=5)
 
-default_delay = tk.StringVar()
-default_delay.set(minutes[-1])
-delay = ttk.OptionMenu(root, default_delay, *minutes)
-delay.pack()
+# Timer type option
+label_timer_type = Label(
+    window,
+    text="When timer ends...",
+    fg="black",
+    bg="pink")
+label_timer_type.grid(column=1, row=0, columnspan=2)
+
+timer_type = StringVar()
+R_Sleep = Radiobutton(window, text="Sleep", var=timer_type, value="-s")
+R_Shutdown = Radiobutton(window, text="Shutdown", var=timer_type, value="-h")
+R_Restart = Radiobutton(window, text="Restart", var=timer_type, value="-r")
+timer_type.set("-s")
+
+R_Sleep.config(fg="black", bg="pink")
+R_Shutdown.config(fg="black", bg="pink")
+R_Restart.config(fg="black", bg="pink")
+
+R_Sleep.grid(column=1, row=1, sticky=W, columnspan=2)
+R_Shutdown.grid(column=1, row=2, sticky=W, columnspan=2)
+R_Restart.grid(column=1, row=3, sticky=W, columnspan=2)
 
 # Start timer button
-label_run = tk.Label(
+label_run = Label(
     text="Start timer",
-    fg="black",
-    bg="pink"
+    foreground="black",
+    background="pink"
 )
-label_run.pack()
+label_run.grid(column=0, row=4, columnspan=2, padx=5, pady=(10,0))
 
-button_run = tk.Button(root, text="Go", command=sleep, highlightbackground="pink")
-button_run.pack()
+button_run = Button(window, text="Go", command=sleep, highlightbackground="pink")
+button_run.grid(column=0, row=5, columnspan=2)
 
 if __name__ == "__main__":
-    root.mainloop()
+    window.mainloop()
